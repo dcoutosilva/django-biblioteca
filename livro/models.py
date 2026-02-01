1599515a0ad8d8fab00a4ff70151293ea2a252b8
danilo <d.couto.silva@gmail.com>
2026-02-01 09:58:48 -0300

    alterando models

--- livro/models.py
+++ livro/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 
+class Emprestimo(models.Model):
+    nome_emprestimo = models.CharField(max_length=100)
+    livro_emprestado = models.CharField(max_length=100)
+    data_emprestimo = models.DateField()
+    data_devolucao = models.DateField()
+    tempo_duracao = models.DateField()
+
 class Livros(models.Model):
     nome = models.CharField(max_length=100) 
     autor = models.CharField(max_length=100)                   
@@ -9,4 +16,9 @@ class Livros(models.Model):
     isbn = models.CharField(max_length=13)
     quantidade = models.IntegerField()
     disponivel = models.BooleanField(default=True)
-    
+    data_cadastro = models.DateField()
+    emprestado = models.BooleanField(default=False)
+    data_emprestimo = models.DateField()
+    data_devolucao = models.DateField()
+    nome_emprestimo = models.CharField(max_length=100)
+    tempo_duracao = models.DateField()    
